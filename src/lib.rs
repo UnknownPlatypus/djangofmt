ddef79aca35bf170a55f8a612b5d9337c3470ad0
Thibaut Decombe <thibaut.decombe@gmail.com>
2024-05-19 18:38:34 +0200

    Add base run command

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use std::process::ExitCode;
+
+use anyhow::Result;
+
+#[derive(Copy, Clone)]
+pub enum ExitStatus {
+    /// Command was successful and there were no errors.
+    Success,
+    /// Command was successful but there were errors.
+    Failure,
+    /// Command failed.
+    Error,
+}
+
+impl From<ExitStatus> for ExitCode {
+    fn from(status: ExitStatus) -> Self {
+        match status {
+            ExitStatus::Success => ExitCode::from(0),
+            ExitStatus::Failure => ExitCode::from(1),
+            ExitStatus::Error => ExitCode::from(2),
+        }
+    }
+}
+
+pub fn run() -> Result<ExitStatus> {
+    Ok(ExitStatus::Success)
+}
