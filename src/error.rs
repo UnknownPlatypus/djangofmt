e39f71a0c90086cc5f0080059b2dd201c37cf006
Thibaut Decombe <68703331+UnknownPlatypus@users.noreply.github.com>
2025-11-24 22:12:07 +0100

    refactor(error): Remove `anyhow` and use custom error enum (#100)
    * Add error module
    
    * Remove `anyhow` usage and update code to take advantage of `?` operator

---
+++ src/error.rs
@@ -0,0 +1,22 @@
+use derive_more::{Display, From};
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Debug, Display, From)]
+#[display("{self:?}")]
+pub enum Error {
+    #[from]
+    FormatCommand(crate::commands::format::FormatCommandError),
+    // -- Externals
+    #[from]
+    Io(std::io::Error),
+
+    #[from]
+    SerdeJson(serde_json::Error),
+}
+
+// region:    --- Error Boilerplate
+
+impl std::error::Error for Error {}
+
+// endregion: --- Error Boilerplate
