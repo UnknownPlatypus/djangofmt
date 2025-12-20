4e92927ac3f3de5b2629ddcda446bd3b7462357f
Thibaut Decombe <68703331+UnknownPlatypus@users.noreply.github.com>
2025-12-20 19:05:03 +0100

    perf(allocator): Use jemalloc on linux (#128)
    * Switch to { version = ... } pattern
    
    * Add jemmalloc behind a feature (but enabled by default)

--- crates/djangofmt/src/main.rs
+++ crates/djangofmt/src/main.rs
@@ -6,6 +6,24 @@ use colored::Colorize;
 use djangofmt::args::{Args, Commands};
 use djangofmt::{ExitStatus, run};
 
+// We use jemalloc for performance reasons.
+// This has to be kept in sync with the Cargo.toml file section that declares a dependency on tikv-jemallocator.
+#[cfg(all(
+    not(target_os = "macos"),
+    not(target_os = "windows"),
+    not(target_os = "openbsd"),
+    not(target_os = "aix"),
+    not(target_os = "android"),
+    any(
+        target_arch = "x86_64",
+        target_arch = "aarch64",
+        target_arch = "powerpc64",
+        target_arch = "riscv64"
+    ),
+    feature = "use-jemalloc"
+))]
+#[global_allocator]
+static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 #[must_use]
 pub fn main() -> ExitCode {
     let args = Args::parse();
