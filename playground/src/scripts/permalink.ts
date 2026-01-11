cab8ff048858cc8f4bb0d799ff30d0ae7934d281
Thibaut Decombe <thibaut.decombe@gmail.com>
2026-01-11 21:38:47 +0100

    Add a "Open an issue on Github" button

--- playground/src/scripts/permalink.ts
+++ playground/src/scripts/permalink.ts
@@ -1,10 +1,15 @@
+import { createPatch } from "diff";
 import LZString from "lz-string";
 import { writeClipboardText } from "./clipboard";
 
-function savePermalinkToClipboard(code: string, mode: string, width: number, indent: number): void {
+function createPermalink(code: string, mode: string, width: number, indent: number): string {
   const compressed = LZString.compressToEncodedURIComponent(code);
   const params = new URLSearchParams({ mode: mode, width: String(width), indent: String(indent), code: compressed });
-  const url = `${window.location.origin}${window.location.pathname}?${params.toString()}`;
+  return `${window.location.origin}${window.location.pathname}?${params.toString()}`;
+}
+
+function savePermalinkToClipboard(code: string, mode: string, width: number, indent: number): void {
+  const url = createPermalink(code, mode, width, indent);
   writeClipboardText(url);
 }
 
@@ -25,4 +30,22 @@ function parsePermalinkCode(): string | null {
   }
 }
 
-export { parsePermalinkCode, savePermalinkToClipboard };
+function openGithubIssue(source: string, mode: string, width: number, indent: number, formatted: string) {
+  const diff = createPatch("template.j2", source, formatted, "", "", { context: 3 });
+  const link = createPermalink(source, mode, width, indent);
+  const body = `<!--
+Describe what is not working.
+-->
+
+\`\`\`diff
+${diff}
+\`\`\`
+
+See [playground](${link})`;
+
+  const url = new URL("https://github.com/unknownplatypus/djangofmt/issues/new");
+  url.searchParams.set("body", body);
+  window.open(url, "_blank");
+}
+
+export { openGithubIssue, parsePermalinkCode, savePermalinkToClipboard };
