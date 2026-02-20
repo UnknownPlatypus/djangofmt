4917ca736e1f10e5cfea50f794af82bf0323b8d4
Thibaut Decombe <thibaut.decombe@gmail.com>
2026-02-21 00:55:34 +0100

    Fix clippy

--- crates/djangofmt/src/commands/format.rs
+++ crates/djangofmt/src/commands/format.rs
@@ -7,7 +7,7 @@ use std::fs::File;
 use std::io::Write;
 use std::path::{Path, PathBuf};
 use std::time::Instant;
-use std::{env, io, u32};
+use std::{env, io};
 use tracing::{debug, error, info};
 
 use crate::ExitStatus;
