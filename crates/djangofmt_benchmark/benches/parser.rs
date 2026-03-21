1e02cbc4f32e6cb61aba4fc607dc4a130fe4fdcb
Thibaut Decombe <thibaut.decombe@gmail.com>
2026-03-22 00:47:13 +0100

    feat(bench): add linter and parser benchmarks with throughput counters

--- crates/djangofmt_benchmark/benches/parser.rs
+++ crates/djangofmt_benchmark/benches/parser.rs
@@ -1,12 +1,9 @@
-use djangofmt::{
-    commands::format::{FormatterConfig, format_text},
-    line_width::{IndentWidth, LineLength, SelfClosing},
-};
 use djangofmt_benchmark::{
     DJANGO_TEMPLATE_ATTR_DENSE, DJANGO_TEMPLATE_DEEPLY_NESTED, DJANGO_TEMPLATE_FORM_HEAVY,
     DJANGO_TEMPLATE_LARGE, DJANGO_TEMPLATE_SMALL, DJANGO_TEMPLATE_TAG_DENSE,
     DJANGO_TEMPLATE_WITH_SCRIPT_AND_STYLE_TAGS, JINJA_TEMPLATE_LARGE, TestFile,
 };
+use markup_fmt::parser::Parser;
 
 fn main() {
     divan::main();
@@ -22,22 +19,15 @@ fn main() {
   &DJANGO_TEMPLATE_ATTR_DENSE,
   &JINJA_TEMPLATE_LARGE]
 )]
-fn format_templates(bencher: divan::Bencher, template: &'static TestFile) {
-    let config = FormatterConfig::new(
-        LineLength::default(),
-        IndentWidth::default(),
-        None,
-        SelfClosing::default(),
-    );
-
+fn parse_templates(bencher: divan::Bencher, template: &'static TestFile) {
     bencher
         .counter(divan::counter::BytesCount::of_str(template.code))
         .bench(|| {
-            format_text(
+            let mut parser = Parser::new(
                 divan::black_box(template.code),
-                divan::black_box(&config),
-                divan::black_box(template.profile),
-            )
-            .expect("Formatting to succeed")
+                divan::black_box(template.profile.into()),
+                vec![],
+            );
+            parser.parse_root().expect("Parsing to succeed")
         });
 }
--- crates/djangofmt_benchmark/resources/README.md
+++ crates/djangofmt_benchmark/resources/README.md
@@ -8,6 +8,9 @@ The following files are included:
 - [`django/tests/utils_tests/files/strip_tags1.html`](https://github.com/django/django/blob/922c4cf972e04b1ce7ecee592231106724dcfd09/tests/utils_tests/files/strip_tags1.html)
 - [`django/django/contrib/admin/templates/admin/404.html`](https://github.com/django/django/blob/922c4cf972e04b1ce7ecee592231106724dcfd09/django/contrib/admin/templates/admin/404.html)
 - [`django/django/technical_500.html`](https://github.com/django/django/blob/922c4cf972e04b1ce7ecee592231106724dcfd09/django/views/templates/technical_500.html)
+- [`django/django/contrib/admin/templates/admin/change_form.html`](https://github.com/django/django/blob/35dab0ad9ee2ed23101420cb0f253deda2818191/django/contrib/admin/templates/admin/change_form.html)
+- [`wagtail/admin/templates/wagtailadmin/login.html`](https://github.com/wagtail/wagtail/blob/6e809784bda871df1bd5987833870e22cb1af940/wagtail/admin/templates/wagtailadmin/login.html)
+- [`wagtail/admin/templates/wagtailadmin/shared/headers/slim_header.html`](https://github.com/wagtail/wagtail/blob/6e809784bda871df1bd5987833870e22cb1af940/wagtail/admin/templates/wagtailadmin/shared/headers/slim_header.html)
 
 The files are included in the `resources` directory to allow running benchmarks offline and for simplicity.
 They're licensed according to their original licenses (see links).
