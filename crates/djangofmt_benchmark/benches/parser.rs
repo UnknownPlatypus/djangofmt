2eab77b5cb9bf5bdcd4d6a802238fc27419021cc
Thibaut Decombe <68703331+UnknownPlatypus@users.noreply.github.com>
2026-06-01 22:13:45 +0200

    refactor(test): Share benchmark template list via `ALL_TEMPLATES` const (#321)

--- crates/djangofmt_benchmark/benches/parser.rs
+++ crates/djangofmt_benchmark/benches/parser.rs
@@ -1,26 +1,11 @@
-use djangofmt_benchmark::{
-    DJANGO_TEMPLATE_ATTR_DENSE, DJANGO_TEMPLATE_DEEPLY_NESTED, DJANGO_TEMPLATE_FORM_HEAVY,
-    DJANGO_TEMPLATE_LARGE, DJANGO_TEMPLATE_SMALL, DJANGO_TEMPLATE_TAG_DENSE,
-    DJANGO_TEMPLATE_WITH_SCRIPT_AND_STYLE_TAGS, JINJA_TEMPLATE_FORM_HEAVY, JINJA_TEMPLATE_LARGE,
-    TestFile,
-};
+use djangofmt_benchmark::{ALL_TEMPLATES, TestFile};
 use markup_fmt::parser::Parser;
 
 fn main() {
     divan::main();
 }
 
-#[divan::bench(args = [
-  &DJANGO_TEMPLATE_SMALL,
-  &DJANGO_TEMPLATE_WITH_SCRIPT_AND_STYLE_TAGS,
-  &DJANGO_TEMPLATE_LARGE,
-  &DJANGO_TEMPLATE_DEEPLY_NESTED,
-  &DJANGO_TEMPLATE_TAG_DENSE,
-  &DJANGO_TEMPLATE_FORM_HEAVY,
-  &DJANGO_TEMPLATE_ATTR_DENSE,
-  &JINJA_TEMPLATE_FORM_HEAVY,
-  &JINJA_TEMPLATE_LARGE]
-)]
+#[divan::bench(args = ALL_TEMPLATES)]
 fn parse_templates(bencher: divan::Bencher, template: &'static TestFile) {
     bencher
         .counter(divan::counter::BytesCount::of_str(template.code))
